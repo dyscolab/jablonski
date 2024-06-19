81c660ccdbae30dce0062013adb0d104ad3914d0
Hernan Grecco <hgrecco@gmail.com>
2024-06-19 22:28:25 +0200

    style: improve docstring style

--- src/jablonski/__init__.py
+++ src/jablonski/__init__.py
@@ -2,9 +2,9 @@
     jablonski
     ~~~~~~~~~
 
-    Write and simulate ODE systems to describe the transitions in molecular systems.
+    Write and simulate ODE systems to describe the transitions in spectroscopic
+    molecular systems.
 
-
-    :copyright: 2024 by redpipy Authors, see AUTHORS for more details.
+    :copyright: 2024 by jablonski Authors, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
