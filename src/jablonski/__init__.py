f2434f62e7fdad270f265e6981d1e65ae7eed1af
Hernan Grecco <hgrecco@gmail.com>
2024-07-29 23:37:48 -0300

    feat: adjust imports

--- src/jablonski/__init__.py
+++ src/jablonski/__init__.py
@@ -8,3 +8,19 @@
     :copyright: 2024 by jablonski Authors, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
+
+from poincare import Parameter, assign
+
+from . import transitions, util
+from .states import SingletState, SpectroscopicSystem, TripletState, initial
+
+__all__ = [
+    "transitions",
+    "util",
+    "SpectroscopicSystem",
+    "initial",
+    "TripletState",
+    "SingletState",
+    "assign",
+    "Parameter",
+]
